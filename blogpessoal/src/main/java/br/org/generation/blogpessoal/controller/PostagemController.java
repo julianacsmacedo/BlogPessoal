3bc0136a7ed3b890a56584f5f0eace536d15522e
julianacsmacedo <julianamacedo2008@gmail.com>
2021-10-29 15:14:42 -0300

    atualizando blog pessoal

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/controller/PostagemController.java
@@ -3,9 +3,15 @@ package br.org.generation.blogpessoal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +19,51 @@ import br.org.generation.blogpessoal.model.Postagem;
 import br.org.generation.blogpessoal.repository.PostagemRepository;
 
 @RestController
-@RequestMapping("/postagens")
+@RequestMapping("/postagens") 
 @CrossOrigin(origins = "*", allowedHeaders = "*") //libera o acesso do front no back
 public class PostagemController {
 	
+		
 	@Autowired //Cria uma injeção de dependencia - trasnfere responsabilidade para o Spring
 	private PostagemRepository postagemRepository;
 	
-	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll() //ResponseEntity -classe responsável por responder nossa requisição
+	@GetMapping //Cria uma injeção de dependencia - trasnfere responsabilidade para o Spring
+	public ResponseEntity<List<Postagem>> getAll () //ResponseEntity - classe responsável por responder nossa requisição
 	{
-		return ResponseEntity.ok(postagemRepository.findAll());
-		//select * from tb_postagens;
+		return ResponseEntity.ok(postagemRepository.findAll()); 
 	}
+	
+	//criação de uma variavel de caminho: "/[variavel]"
+	//@PathVariable: pega do caminho e retorna para
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable long id) 
+	{
+		return postagemRepository.findById(id)
+			.map(resp -> ResponseEntity.ok(resp))
+			.orElse(ResponseEntity.notFound().build());
+	}
+	
+	//Consulta por titulo
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	//Cria uma nova postagem
+	@PostMapping
+	public ResponseEntity<Postagem> postPostagem (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> putPostagem (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+	}
+			
+	//Deleta
+	@DeleteMapping("/{id}")
+	public void deletePostagem(@PathVariable long id) {
+		postagemRepository.deleteById(id);
+	}	
+
 }
