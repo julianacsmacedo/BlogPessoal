bc88125bc86e554a998e01b76006d29c5df5f37e
julianacsmacedo <julianamacedo2008@gmail.com>
2021-11-11 18:49:24 -0300

    Spring Security

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_postagens") 
@@ -31,6 +34,14 @@ public class Postagem {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis()); // Date Timestamp()
+	
+	@ManyToOne
+	@JsonIgnoreProperties(value = "postagem")
+	private Usuario usuario;
+	
+	@ManyToOne
+	@JsonIgnoreProperties(value = "postagem")
+	private Tema tema;
 
 	
 	public long getId() {
@@ -64,5 +75,23 @@ public class Postagem {
 	public void setData(Date data) {
 		this.data = data;
 	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	
+	
 
 }
